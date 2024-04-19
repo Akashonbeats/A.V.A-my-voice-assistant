9ac7daf5f1d5152b3923468c76864e4303c84b23
Akashonbeats <akash.s1204@gmail.com>
2024-04-19 10:40:21 +0530

    Rough connection added by the Gemini API.

--- Project Ava.py
+++ Project Ava.py
@@ -1,1 +1,8 @@
-print("Hi, I am Ava. How are you?")
+import google.generativeai as genai
+
+GOOGLE_API_KEY = 'AIzaSyAL5xzHxg0hQubxI_hyooA5aQhZmPQ8y1k'
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-1.0-pro-latest')
+
+response = model.generate_content(input('Ask Ava: '))
+print(response)
